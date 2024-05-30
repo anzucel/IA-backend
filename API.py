ecda0118bb7b11748c48fe640ef8b687e194f4a1
luciale <lucialecabrerao@gmail.com>
2024-05-30 00:10:05 -0600

    add cors

--- API.py
+++ API.py
@@ -1,28 +1,28 @@
-from flask import Flask
-# from flask_pymongo import pymongo
-# from pymongo import MongoClient
-from flask import Flask, Response
-import requests
-from flask import request
-from flask import jsonify
-from pymongo.errors import ConnectionFailure
-from bson.json_util import dumps
+import asyncio
+import aiohttp
 
-import User
-# from bson.json_util import dumps
-import json
-
-from pymongo import MongoClient
+from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
-from model import Model
-from naiveBayes import NaiveBayes
+from pymongo.errors import ConnectionFailure
 
 app = Flask(__name__)
-
 app.config["MONGO_URI"] = "mongodb+srv://admin:12345@cluster0.prvnm39.mongodb.net/dbexample?retryWrites=true&w=majority&appName=Cluster0"
 mongo = PyMongo(app)
 
+from model import Model
+from naiveBayes import NaiveBayes
+
 model = Model()
+
+async def fetch_movie_details(session, movie_title):
+    url = f'http://www.omdbapi.com/?apikey=ccbef632&t={movie_title}'
+    async with session.get(url) as response:
+        return await response.json()
+
+async def fetch_all_movie_details(movie_titles):
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch_movie_details(session, title) for title in movie_titles]
+        return await asyncio.gather(*tasks)
 
 @app.route('/check_mongo')
 def check_mongo():
@@ -49,7 +49,7 @@ def register():
     mongo.db.user.insert_one(data)
     return jsonify({'status': 'success'}), 200
 
-#iniciar sesión
+# Iniciar sesión
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
@@ -59,14 +59,13 @@ def login():
         return jsonify({'message': 'Bad Request'}), 500
     else:
         if existing_user:
-            # valida contraseña
+            # Validar contraseña
             if data['password'] == existing_user['password']:
                 return jsonify({"status": "success"}), 200
             else:
                 return jsonify({"message": "incorrect password"}), 401
         else:
             return jsonify({"error": "User not found, try again!"}), 404
-
 
 # Obtener publisher
 @app.route('/publishers', methods=['GET'])
@@ -78,32 +77,31 @@ def getPublisher():
     else:
         return jsonify({'message': 'Publishers not found'}), 400
 
-
-# Obtener peliculas
+# Obtener películas
 @app.route('/movies', methods=['GET'])
 def getMovies():
     movies_collection = mongo.db.movie.find({}, {"_id": 0})
     movies = list(movies_collection)
 
     if movies:
-        return app.response_class(dumps({'movies': movies}), mimetype='application/json'), 200
+        return jsonify({'movies': movies}), 200
     else:
-        return jsonify({'message': 'Publishers not found'}), 400
+        return jsonify({'message': 'Movies not found'}), 400
 
 @app.route('/users', methods=['GET'])
 def getUsers():
     users_collection = mongo.db.user.find({}, {"_id": 0})
     users = list(users_collection)
     if users:
-        return app.response_class(dumps({'users': users}), mimetype='application/json'), 200
+        return jsonify({'users': users}), 200
     else:
-        return jsonify({'message': 'Publishers not found'}), 400
+        return jsonify({'message': 'Users not found'}), 400
 
-# realiza la clasificacion segun la review
+# Realizar la clasificación según la reseña
 @app.route('/predict', methods=['POST'])
 def predict():
     res = model.runExamples(request.json['rotten_link'], request.json['publisher'], request.json['review'],
-                                 request.json['user'])
+                             request.json['user'])
     if res:
         data = {}
         data['movie_title'] = request.json['movie_title']
@@ -111,14 +109,14 @@ def predict():
         data['review'] = request.json['review']
         data['review_type'] = res
 
-        # Guardar datos review
+        # Guardar datos de la reseña
         mongo.db.review.insert_one(data)
 
         return jsonify({'classification': res}), 200
     else:
-        return jsonify({'message': 'Publishers not found'}), 400
+        return jsonify({'message': 'Prediction failed'}), 400
 
-# consultar reviews por pelicula
+# Consultar reseñas por película
 @app.route('/get-reviews', methods=['POST'])
 def getReviews():
     data = request.get_json()
@@ -128,27 +126,28 @@ def getReviews():
     if reviews_collection:
         return jsonify({'reviews': reviews}), 200
     else:
-        return jsonify({'message': 'Publishers not found'}), 400
+        return jsonify({'message': 'Reviews not found'}), 400
 
 @app.route('/movies-detail', methods=['GET'])
 def getMoviesDetail():
     movies_collection = mongo.db.movie.find({}, {"_id": 0})
 
     if movies_collection:
-        for movie in movies_collection:
-            url = f'http://www.omdbapi.com/?apikey=ccbef632&t={movie["movie_title"]}'
-            response = requests.get(url)
-            data = response.json()
-            movie["year"] = data.get("Year")
-            movie["plot"] = data.get("Plot")
-            movie["actors"] = data.get("Actors")
-            movie["poster"] = data.get("Poster")
+        movie_titles = [movie["movie_title"] for movie in movies_collection]
+        movie_details = asyncio.run(fetch_all_movie_details(movie_titles))
+
+        for movie, details in zip(movies_collection, movie_details):
+            movie["year"] = details.get("Year")
+            movie["plot"] = details.get("Plot")
+            movie["actors"] = details.get("Actors")
+            movie["poster"] = details.get("Poster")
 
             mongo.db.movie.update_one({"movie_title": movie["movie_title"]},
                                       {"$set": movie}, upsert=True)
+
         return jsonify({'classification': "success"}), 200
     else:
-        return jsonify({'message': 'Publishers not found'}), 400
+        return jsonify({'message': 'Movies not found'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
